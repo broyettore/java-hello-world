3167bf9a683b0cc2c1a7e0cbecf2f382042150c8
broyettore <broyettore@gmail.com>
2023-07-06 18:42:32 +0200

    test with arrays and for/foreach in java

--- Main.java
+++ Main.java
@@ -1,20 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       String[][] people = {
+               {"roy", "lucy", "joy"},
+               {"yaru", "yuri", "yerim"},
+               {"john", "eric", "fary"},
+       };
 
-        System.out.println("What is your first name ?");
-        String name = scanner.nextLine();
-
-        System.out.println("What is your last name ?");
-        String lastName = scanner.nextLine();
-
-        System.out.println("What is your age ?");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("My name is " + name + " " + lastName );
-        System.out.println("I'm " + age + " " + "years old.");
+       for (int i = 0; i < people.length; i++) {
+           System.out.println();
+           for (String name: people[i]) {
+               System.out.print(name + " ");
+           }
+       }
     }
 }
