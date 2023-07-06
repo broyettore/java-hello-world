67c11075b4e28db78db6baf85e6c99ff387bbb23
broyettore <broyettore@gmail.com>
2023-07-06 16:38:43 +0200

    first import in java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("What is your first name ?");
+        String name = scanner.nextLine();
+
+        System.out.println("What is your last name ?");
+        String lastName = scanner.nextLine();
+
+        System.out.println("What is your age ?");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("My name is " + name + " " + lastName );
+        System.out.println("I'm " + age + " " + "years old.");
     }
 }
