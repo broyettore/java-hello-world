0af4f717413bb9401243ddcf694795aa1e8deba0
broyettore <broyettore@gmail.com>
2023-07-10 20:31:02 +0200

    test OOP in java

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       String[][] people = {
-               {"roy", "lucy", "joy"},
-               {"yaru", "yuri", "yerim"},
-               {"john", "eric", "fary"},
-       };
+        Persona test1 = new Persona();
 
-       for (int i = 0; i < people.length; i++) {
-           System.out.println();
-           for (String name: people[i]) {
-               System.out.print(name + " ");
-           }
-       }
+        System.out.println(test1.name + test1.age + test1.favoriteColor);
+        test1.greet();
     }
 }
---
+++ Persona.java
@@ -0,0 +1,10 @@
+public class Persona {
+
+    String name = "Hero";
+    String surname = "Jones";
+    int age = 25;
+    String favoriteColor = "Black";
+    void greet() {
+        System.out.println(name + " " + surname);
+    }
+}
